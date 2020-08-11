2dffa35b23093d4c616715735e79a27c821af503
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-11 12:17:01 +1000

    style(difference_of_squares.cr): main file formatted with crystal tool format command
    n

--- src/difference_of_squares.cr
+++ src/difference_of_squares.cr
@@ -1,26 +1,26 @@
 # Please implement your solution to difference-of-squares in this file
-module Squares 
-    def self.square_of_sum(num : Int32) : Int32
-        count = 0
-        result = 0
-        while count <= num
-            result += count 
-            count += 1 
-        end
-        return result**2
+module Squares
+  def self.square_of_sum(num : Int32) : Int32
+    count = 0
+    result = 0
+    while count <= num
+      result += count
+      count += 1
     end
+    return result**2
+  end
 
-    def self.sum_of_squares(num : Int32) : Int32
-        count = 0
-        result = 0
-        while count <= num
-            result += count**2 
-            count += 1 
-        end
-        return result
+  def self.sum_of_squares(num : Int32) : Int32
+    count = 0
+    result = 0
+    while count <= num
+      result += count**2
+      count += 1
     end
+    return result
+  end
 
-    def self.difference_of_squares(num : Int32) : Int32
-        (square_of_sum num) - (sum_of_squares num)
-    end
+  def self.difference_of_squares(num : Int32) : Int32
+    (square_of_sum num) - (sum_of_squares num)
+  end
 end
